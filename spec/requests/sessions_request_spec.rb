0e110bf5c937ea05f80cf700106c3720bc925f30
edlahaba <ernestohlacalle@gmail.com>
2020-07-15 16:26:37 +0200

    Removed login method in session request test
    Changelog
    ---------
    * Remove login method from session request test

--- spec/requests/sessions_request_spec.rb
+++ spec/requests/sessions_request_spec.rb
@@ -85,13 +85,4 @@ RSpec.describe "Sessions", type: :request do
       end
     end
   end
-
-  def login(user)
-    post login_path, params: {
-      session: {
-        email: user.email,
-        password: user.password,
-      },
-    }
-  end
 end
